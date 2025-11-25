f6ee854674a77428f8f54ed94e43e8c630a73d2e
shengqu12 <shengq@andrew.cmu.edu>
2025-11-25 14:18:23 -0500

    register

--- climate/admin.py
+++ climate/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
-
+from .models import Community, ClimateRecord
 # Register your models here.
+
+@admin.register(Community)
+class CommunityAdmin(admin.ModelAdmin):
+    list_display = ('name','borough','latitude','longitude')
+    search_fields=('name','borough')
+
+@admin.register(ClimateRecord)
+class ClimateRecordAdmin(admin.ModelAdmin):
+    list_display = ("community", "date", "temperature", "precipitation", "pollution_index")
+    list_filter = ("community", "date")
+    search_fields = ("community__name",)
